88639ad75cba138097d85cbdb31ef3d20ff08c30
Tamilarasu93454 <tamilarasushanmugavel@gmail.com>
2023-10-24 20:42:03 +0530

    image name changed

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import './index.css';
-import about from "./images/about.jpg";
+import back from "./images/back.jpg";
 function Index(){
     return(
     <div className="Container">
       <div className="Backimage">
-       <image src={about} alt="aboutpage"/> 
-      <div className="Header"></div>
+       <image src={back} alt="aboutpage" width="100%" height="50px"/> 
+       {/* <div className="Header"></div> */}
       </div>
     </div>
     )
