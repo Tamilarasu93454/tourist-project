60be145ead70df2ad2b99afaa23f29e6fbd88445
Tamilarasu93454 <tamilarasushanmugavel@gmail.com>
2023-10-24 20:36:09 +0530

    image is added

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import './index.css';
+import about from "./images/about.jpg";
+function Index(){
+    return(
+    <div className="Container">
+      <div className="Backimage">
+       <image src={about} alt="aboutpage"/> 
+      <div className="Header"></div>
+      </div>
+    </div>
+    )
+}
+export default Index;
